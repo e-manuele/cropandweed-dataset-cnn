dc2df0432d1a7ba9831f21c42655240dbb95340b
Dioico <74198922+Dioico@users.noreply.github.com>
2023-12-15 14:54:30 +0100

    optimization test 1

--- Visualizer.py
+++ Visualizer.py
@@ -7,11 +7,15 @@ if __name__ == '__main__':
     image_path = 'ave-0037-0001.jpg'
     target_path = 'data/images/' + image_path
     model = YOLO(best_model_path)
+
+    inference = model(target_path, save=True, save_crop=True,visualize=True)
+    '''
+
     results = model(target_path)  # , conf=.1)
     shutil.copy(target_path, "target.jpg")
-
-    for r in results:
-        im_array = r.plot()
+    '''
+    for data in inference:
+        im_array = data.plot()
         im = Image.fromarray(im_array[..., ::-1])
         im.show()
         im.save('result.jpg')
--- YOLO_cnw.py
+++ YOLO_cnw.py
@@ -1,8 +1,16 @@
 from ultralytics import YOLO
 
 model = YOLO('yolov8n.pt')
-results = model.train(data='cropandweed.yaml', epochs=5)
+results = model.train(data='cnw.yaml', epochs=5)
 
 path = model.export(format='onnx')
 model.save("yolov8n_cnw.pt")
 val = model.val()
+
+'''
+
+optimizer: 'optimizer=auto' found, ignoring 'lr0=0.01' and 'momentum=0.937' and determining best 'optimizer', 'lr0' and 'momentum' automatically... 
+optimizer: AdamW(lr=0.001667, momentum=0.9) with parameter groups 57 weight(decay=0.0), 64 weight(decay=0.0005), 63 bias(decay=0.0)
+Image sizes 640 train, 640 val
+
+'''
